fa9fdba43d919e2584f11335924f7ad3c751c63e
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-01-23 12:25:05 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,24 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+abstract class Figure2D{
+    abstract fun volume():Double
+    abstract fun field():Double
+
+    fun info(){
+        print("Info")
+    }
+}
+
+class Square(val a: Double):Figure2D(){
+    override fun volume(): Double {
+        return a*a
+    }
+    override fun field(): Double {
+        return 4*a
+    }
+}
+
+class Rectangle(val a: Double, val b: Double){
+
+}
+
+fun main() {
 }
