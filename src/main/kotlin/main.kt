fa0951f1df181fdbf38b735b1776ff27f32ab6ee
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-01-23 12:26:52 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,24 +1,37 @@
-abstract class Figure2D{
-    abstract fun volume():Double
-    abstract fun field():Double
+abstract class Figure2D {
+    abstract fun volume(): Double
+    abstract fun field(): Double
 
-    fun info(){
-        print("Info")
+    fun info() {
+        println("Volume: ${volume()}")
+        println("Field: ${field()}")
     }
 }
 
-class Square(val a: Double):Figure2D(){
+class Square(val a: Double) : Figure2D() {
     override fun volume(): Double {
-        return a*a
+        return a * a
     }
+
     override fun field(): Double {
-        return 4*a
+        return 4 * a
     }
 }
 
-class Rectangle(val a: Double, val b: Double){
+class Rectangle(val a: Double, val b: Double) {
 
 }
 
 fun main() {
+    val figureList = mutableListOf<Figure2D>()
+
+
+    figureList.add(Square(10.0))
+    //figureList.add(Rectangle(10.0,10.0))
+
+    figureList.forEach {
+        it.info()
+        println("------------------")
+    }
+
 }
