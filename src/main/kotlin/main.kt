e8a317d42ef4a593de5c1d5e04c779719686dcf2
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-01-23 13:32:04 +0100

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,14 @@
-abstract class Figure2D {
-    abstract fun area(): Double
-    abstract fun perimeter(): Double
+interface Figure2D {
+    fun area(): Double
+    fun perimeter(): Double
 
-    open fun info() {
+    fun info() {
         println("Area: ${area()}")
         println("Perimeter: ${perimeter()}")
     }
 }
 
-open class Square(val a: Double) : Figure2D() {
+open class Square(val a: Double) : Figure2D {
     override fun area(): Double {
         return a * a
     }
@@ -17,20 +17,20 @@ open class Square(val a: Double) : Figure2D() {
         return 4 * a
     }
 
-    override fun info(){
+    override fun info() {
         println("---------Square---------")
         println("a: $a")
         super.info()
     }
 }
 
-class Rectangle(val a: Double, val b: Double):Figure2D() {
+class Rectangle(val a: Double, val b: Double) : Figure2D {
     override fun area(): Double {
-        return a*b
+        return a * b
     }
 
     override fun perimeter(): Double {
-        return 2*a+2*b
+        return 2 * a + 2 * b
     }
 
     override fun info() {
@@ -40,13 +40,13 @@ class Rectangle(val a: Double, val b: Double):Figure2D() {
     }
 }
 
-class Circle(val r: Double): Figure2D(){
+class Circle(val r: Double) : Figure2D {
     override fun area(): Double {
-        return Math.PI*r*r
+        return Math.PI * r * r
     }
 
     override fun perimeter(): Double {
-        return 2*Math.PI*r
+        return 2 * Math.PI * r
     }
 
     override fun info() {
@@ -57,29 +57,75 @@ class Circle(val r: Double): Figure2D(){
 
 }
 
-class Base(val figure2D: Figure2D){
-    fun area():Double{
+class Base(val figure2D: Figure2D, val material:String) {
+    fun area(): Double {
         return figure2D.area()
     }
 
-    fun perimeter():Double{
-        return figure2D.area()
+    fun perimeter(): Double {
+        return figure2D.perimeter()
+    }
+
+    fun material():String{
+        return material
     }
 }
+
+class newBase(val figure2D: Figure2D, val material: String) : Figure2D by figure2D{
+    fun material():String{
+        return material
+    }
+
+    override fun info(){
+        println("area ${area()}")
+        println("perimeter ${perimeter()}")
+    }
+}
+
+class Figure3D(val base: Base, val h: Double) {
+    fun volume(): Double {
+        return base.area() * h
+    }
+
+    fun area(): Double {
+        return h * base.perimeter() + 2 * base.area()
+    }
+}
+
+//To może być troszkę trudniejsze...
+class newFigure3D(val base: Figure2D, val h: Double) : Figure2D by base {
+    fun volume(): Double {
+        return area() * h
+    }
+
+    fun figure3DArea(): Double {
+        return h * perimeter() + 2 * area()
+    }
+}
+
 
 fun main() {
     val figureList = mutableListOf<Figure2D>()
 
     figureList.add(Square(10.0))
-    figureList.add(Rectangle(10.0,10.0))
+    figureList.add(Rectangle(10.0, 10.0))
     figureList.add(Circle(2.0))
 
     figureList.forEach {
         it.info()
     }
 
-    val base = Base(Square(10.0))
+    val base = Base(Square(10.0),"wood")
     println(base.area())
     println(base.perimeter())
+
+    val newBase = newBase(Square(10.0),"wood")
+    println(newBase.area())
+    println(newBase.perimeter())
+
+    val roller = Figure3D(Base(Circle(10.0),"plastic"),5.0)
+    roller.volume()
+
+    val newRoller = newFigure3D(Circle(10.0),5.0)
 
 }
